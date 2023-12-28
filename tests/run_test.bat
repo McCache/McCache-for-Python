478a8a414a180226b741cb2ec25f7b1e84883a61
Edward Lau <elau1004@netscape.net>
2023-12-28 09:41:11 -0800

    Parameter drive the size of the test cluster

--- tests/run_test.bat
+++ tests/run_test.bat
@@ -54,6 +54,7 @@ SET TEST_SLEEP_UNIT=100
 :: Setup the variable TEST_MONKEY_TANTRUM to be passed into the container composer.
 SET TEST_MONKEY_TANTRUM=0
 
+
 :: Start of CLI.
 :SOF_CLI
 IF  /I "TEST_MAX_ENTRIES"   =="%1" GOTO :SET_TEST_MAX_ENTRIES
@@ -61,38 +62,45 @@ IF  /I "TEST_RUN_DURATION"  =="%1" GOTO :SET_TEST_RUN_DURATION
 IF  /I "TEST_SLEEP_SPAN"    =="%1" GOTO :SET_TEST_SLEEP_SPAN
 IF  /I "TEST_SLEEP_UNIT"    =="%1" GOTO :SET_TEST_SLEEP_UNIT
 IF  /I "TEST_MONKEY_TANTRUM"=="%1" GOTO :SET_TEST_MONKEY_TANTRUM
+IF  /I "SIZE"               =="%1" GOTO :SET_TEST_CLUSTER_SIZE
 IF  /I ""                   =="%1" GOTO :EOF_CLI
 
 ECHO Invalid parameter value.  Try the following:
-ECHO    %0  [TEST_MAX_ENTRIES ###] [TEST_RUN_DURATION ###] [TEST_SLEEP_SPAN ###] [TEST_SLEEP_UNIT ###]
+ECHO    %0  [TEST_MAX_ENTRIES ###] [TEST_RUN_DURATION ###] [TEST_SLEEP_SPAN ###] [TEST_SLEEP_UNIT ###] [NODES ###]
 GOTO :EOF_SCRIPT
 
 :SET_TEST_MAX_ENTRIES
-SET TEST_MAX_ENTRIES=%2
+SET  TEST_MAX_ENTRIES=%2
 SHIFT
 SHIFT
 GOTO :SOF_CLI
 
 :SET_TEST_RUN_DURATION
-SET TEST_RUN_DURATION=%2
+SET  TEST_RUN_DURATION=%2
 SHIFT
 SHIFT
 GOTO :SOF_CLI
 
 :SET_TEST_SLEEP_SPAN
-SET TEST_SLEEP_SPAN=%2
+SET  TEST_SLEEP_SPAN=%2
 SHIFT
 SHIFT
 GOTO :SOF_CLI
 
 :SET_TEST_SLEEP_UNIT
-SET TEST_SLEEP_UNIT=%2
+SET  TEST_SLEEP_UNIT=%2
 SHIFT
 SHIFT
 GOTO :SOF_CLI
 
 :SET_TEST_MONKEY_TANTRUM
-SET TEST_MONKEY_TANTRUM=%2
+SET  TEST_MONKEY_TANTRUM=%2
+SHIFT
+SHIFT
+GOTO :SOF_CLI
+
+:SET_TEST_CLUSTER_SIZE
+SET  TEST_CLUSTER_SIZE=%2
 SHIFT
 SHIFT
 GOTO :SOF_CLI
@@ -108,6 +116,7 @@ ECHO    TEST_RUN_DURATION:  %TEST_RUN_DURATION%
 ECHO    TEST_SLEEP_SPAN:    %TEST_SLEEP_SPAN%
 ECHO    TEST_SLEEP_UNIT:    %TEST_SLEEP_UNIT%
 ECHO    TEST_MONKEY_TANTRUM:%TEST_MONKEY_TANTRUM%
+ECHO    TEST_CLUSTER_SIZE:  %TEST_CLUSTER_SIZE%
 ECHO:
 
 :: The following are CLI input parameter you can use to parse out the script name information.
@@ -129,11 +138,18 @@ PUSHD  %ROOTDIR%
 ::
 IF NOT EXIST  log (MD log) ELSE (DEL /q log\*)
 
+:: Initialize a minimum of three nodes.
+SET  NODE01=node01
+SET  NODE02=node02
+SET  NODE03=node03
+
+FOR /L %%v  IN ( 1 ,1 ,%TEST_CLUSTER_SIZE% )  DO  SET NODE0%%v=node0%%v
+
 :: Bring up the cluster of containers and wait until we are done exercising the cache.
 ::
-ECHO Starting the test cluster.
-:: Keep in forgound
-%CONTAINER_EXE% up      &:: Keep in foreground.
+ECHO Starting the test cluster with %TEST_CLUSTER_SIZE% nodes.
+:: Keep in foregound with the maximum of 9 nodes as specified in "docker-compose.yml"
+%CONTAINER_EXE% up  %NODE01% %NODE02% %NODE03% %NODE04% %NODE05% %NODE06% %NODE07% %NODE08% %NODE09%
 
 :: Wait for the test run to be completed in the cluster and test the output log.
 ECHO Run test using the output log from the cluster.
